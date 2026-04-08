ee5b17c9665b65377d8fa009c0066a9fabb5f832
arfath-ali <1290sfaa28@gmail.com>
2026-04-08 18:01:57 +0530

    add return statements

--- server/src/server.ts
+++ server/src/server.ts
@@ -61,44 +61,57 @@ const server = http.createServer(
           parseRequestBody(req, res, () => {
             if (req.url === '/api/signup') {
               signupController(req, res);
+              return;
             }
             if (req.url === '/api/verify-otp') {
               otpVerificationController(req, res);
+              return;
             }
             if (req.url === '/api/resend-otp') {
               resendOTPController(req, res);
+              return;
             }
             if (req.url === '/api/signin') {
               signinController(req, res);
+              return;
             }
             if (req.url === '/api/reset') {
               resetPasswordController(req, res);
+              return;
             }
             if (req.url === '/api/verify-reset-token') {
               verifyResetTokenController(req, res);
+              return;
             }
             if (req.url === '/api/update') {
               updatePasswordController(req, res);
+              return;
             }
             if (req.url === '/api/profile') {
               profileController(req, res);
+              return;
             }
             if (req.url === '/api/delete') {
               deleteAccountController(req, res);
+              return;
             }
           });
         } else if (req.method === 'GET') {
           if (req.url?.startsWith('/api/auth/google/callback')) {
             googleCallbackController(req, res);
+            return;
           }
           if (req.url?.startsWith('/api/auth/google')) {
             googleAuthController(req, res);
+            return;
           }
           if (req.url === '/api/location') {
             locationController(req, res);
+            return;
           }
           if (req.url === '/api/auth/status') {
             getSessionController(req, res);
+            return;
           }
         }
       } else {
@@ -112,6 +125,7 @@ const server = http.createServer(
         }
 
         serveHTML(req, res, indexPath);
+        return;
       }
     } catch (err) {
       console.error('INTERNAL SERVER ERROR:', err);
