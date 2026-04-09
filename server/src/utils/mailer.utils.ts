b724eec53d9dc326bf049d5f64a66e3b350f6b5a
arfath-ali <1290sfaa28@gmail.com>
2026-04-09 13:37:04 +0530

    update mailer

--- server/src/utils/mailer.utils.ts
+++ server/src/utils/mailer.utils.ts
@@ -2,13 +2,15 @@ import nodemailer from 'nodemailer';
 import 'dotenv/config';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
   host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
+  family: 4,
+} as nodemailer.TransportOptions);
 
 export async function sendVerificationEmail(email: string, otp: string) {
   try {
